380418d691da6cd8366873827be69cad8b95e58e
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 15:35:22 +0800

    Update a002.c

--- 期中考試/BMI/a002.c
+++ 期中考試/BMI/a002.c
@@ -1,1 +1,11 @@
-
+//double input
+#include<stdio.h>
+double main(void) {
+	double weight, height, BMI;
+	printf("%s", "Enter your weight(kg):");
+	scanf_s("%lf", &weight);
+	printf("%s", "Enter your height(m):");
+	scanf_s("%lf", &height);
+	BMI = weight/(height * height);
+	printf("%s%.3lf", "Your BMI is ", BMI);
+}
