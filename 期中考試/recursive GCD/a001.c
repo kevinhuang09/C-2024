1ed2fc8199d118beb8a845f16139784d2b155a83
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 19:12:41 +0800

    Update a001.c

--- 期中考試/recursive GCD/a001.c
+++ 期中考試/recursive GCD/a001.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int findGCD(int n1, int n2);
+int main() {
+	printf("%s", "Enter two numbers to calculate GCD:");
+	int n1, n2;
+	scanf_s("%d%d", &n1, &n2);
+	int GCD = findGCD(n1, n2);
+	printf("%s%d\n", "The GCD is ", GCD);
+}
+int findGCD(int n1, int n2) {
+	if (n2 > n1) {
+		int tmp = n1;
+		n1 = n2;
+		n2 = tmp;
+	}
+	if (n2 != 0) {
+		return findGCD(n2, n1 % n2);
+	}
+	else {
+		return n1;
+	}
+}
