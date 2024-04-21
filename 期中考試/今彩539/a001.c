0a6803dceb6497b407c728b2ba62854cdf0c85ef
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 16:25:44 +0800

    Update a001.c

--- 期中考試/今彩539/a001.c
+++ 期中考試/今彩539/a001.c
@@ -1,1 +1,18 @@
-
+//no array
+#include<stdio.h>
+#include<stdlib.h>
+int rd(int n1, int n2) {
+	return n1 + rand() % n2;
+}
+int main() {
+	srand(time(NULL));
+	int ct = 0;
+	while (1) {
+		if (ct == 5) {
+			break;
+		}
+		int result = rd(1, 39);
+		printf("%d ", result);
+		ct++;
+	}
+}
