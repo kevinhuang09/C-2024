d45567ed44687cdd554b2ac2fe38b44cb85e9b8b
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 15:44:13 +0800

    Update a001.c

--- 期中考試/Interest calculator/a001.c
+++ 期中考試/Interest calculator/a001.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+float main(void) {
+	float interest, principal, rate;
+	int day,ct=0;
+	while (1) {
 
+		printf("Enter the pricipal(-1 to end):");
+		scanf_s("%f", &principal);
+
+		if (principal == -1) {
+			break;
+		}
+
+		printf("%s", "Enter the rate:");
+		scanf_s("%f", &rate);
+
+		printf("%s","Enter the day:");
+		scanf_s("%d", &day);
+
+		//calculate
+		interest = (principal * rate * day) / 365;
+
+		printf("%s%f", "The interest is ", interest);
+		puts("");
+
+		ct++;
+	}
+}
