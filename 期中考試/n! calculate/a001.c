44cdb8d7122ba2ae301ddf956624ea30580d3006
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 15:49:14 +0800

    Update a001.c

--- 期中考試/n! calculate/a001.c
+++ 期中考試/n! calculate/a001.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+int function(int n);
+int main(void) {
+	int number;
+	printf("%s", "Enter the number to calculate n!:");
+	scanf_s("%d", &number);
 
+	int result = function(number);
+	printf("%d%s%d",number,"! = ",result);
+}
+int function(int n) {
+	int sum = 1;
+	if (n == 1) {
+		return 1;
+	}
+	else {
+		for (int i = 2; i <= n; i++) {
+			sum *= i;
+		}
+	}
+	return sum;
+}
