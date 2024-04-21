ec8edd18fad1e36f0be292af14311390c35ad434
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 15:53:37 +0800

    Update a001.c

--- 期中考試/e calculate/a001.c
+++ 期中考試/e calculate/a001.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+int function(int n);
+int main(void) {
+	int number;
+	printf("%s", "Enter the number to calculate e:");
+	scanf_s("%d", &number);
 
+	float sum = 1;
+	for (int i = 1; i <= number; i++) {
+		sum += 1.0/ function(i);
+	}
+	printf("%s%.3f","e = ",sum);
+}
+int function(int n) {
+	int sum = 1;
+	if (n == 1) {
+		return 1;
+	}
+	else {
+		for (int i = 2; i <= n; i++) {
+			sum *= i;
+		}
+	}
+	return sum;
+}
