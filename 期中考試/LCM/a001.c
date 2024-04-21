7ab387b5d519964e063fd511a2d0bb16da031c91
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 19:22:33 +0800

    Update a001.c

--- 期中考試/LCM/a001.c
+++ 期中考試/LCM/a001.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+int findGCD(int n1, int n2) {
+	if (n1 < n2) {
+		int tmp = n1;
+		n1 = n2;
+		n2 = tmp;
+	}
+	if (n2 != 0) {
+		return findGCD(n2, n1 % n2);
+	}
+	else {
+		return n1;
+	}
+}
+int main() {
+	printf("%s", "Enter two numbers to calculate LCM:");
+	int n1, n2;
+	scanf_s("%d%d", &n1, &n2);
+	int GCD = findGCD(n1, n2);
+	printf("%s%d\n", "The LCM is ", (n1 * n2 / GCD));
+}
