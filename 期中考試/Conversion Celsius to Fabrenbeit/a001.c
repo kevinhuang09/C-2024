6ad1b1b7460aa3fcc7517413f20813956946a634
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 16:12:58 +0800

    Update a001.c

--- 期中考試/Conversion Celsius to Fabrenbeit/a001.c
+++ 期中考試/Conversion Celsius to Fabrenbeit/a001.c
@@ -1,1 +1,8 @@
-
+//Write a program that converts temperatures from 30~50C to the Fahrenheit scale.
+#include<stdio.h>
+int main() {
+	for (int i = 30; i <= 50; i++) {
+		float F = ((9.0 / 5.0) * i) + 32;
+		printf("%d%s%.3f\n", i, "   ", F);
+	}
+}
