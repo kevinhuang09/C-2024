a4128cc18d45276ec8a0d42e5d034357317643b3
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2024-04-21 16:33:40 +0800

    Update a001.c

--- 期中考試/Coin Tossing/a001.c
+++ 期中考試/Coin Tossing/a001.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+int rd(int n1, int n2) {
+	return n1 + rand() % n2;
+}
+int main() {
+	srand(time(NULL));
+	int ct = 0;
+	int ct1 = 0;
+	int H_ct = 0;
+	int T_ct = 0;
+	while (1) {
+		if (ct == 100) {
+			break;
+		}
+		if (ct1 == 10) {
+			puts("");
+			ct1 = 0;
+		}
+		int result = rd(1, 2);
+		if (result == 1) {
+			printf("%s ", "Heads");
+			H_ct++;
+		}
+		else {
+			printf("%s ","Tails");
+			T_ct++;
+		}
+		
+		ct++; ct1++;
 
+	}
+	puts("");
+	printf("%s%d%s\n", "Heads ", H_ct, " times");
+	printf("%s%d%s\n", "Tails ", T_ct, " times");
+}
